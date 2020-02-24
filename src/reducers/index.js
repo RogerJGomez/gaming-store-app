fbd5becfc1dcd24d68aa39fb73b04576134b210e
RogerJGomez <gomez.roger779@gmail.com>
2020-02-23 20:36:41 -0400

    Reducers structure modified

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -16,7 +16,7 @@ export const reducers = (state = initialState, action) => {
 
         case ADD_PRODUCT:
 
-          var products = [...state.products]
+          var products = JSON.parse(JSON.stringify(state.products))
           var productAdded = products.find(product => product.id === action.product.id)
           
           if(productAdded.stock===0){
@@ -31,7 +31,7 @@ export const reducers = (state = initialState, action) => {
 
           products.sort((productA,productB) => (productA.id<productB.id ? -1 : (productA.rol > productB.rol) ? 1 : 0))
 
-          var cart = [...state.cart]
+          var cart = JSON.parse(JSON.stringify(state.cart))
           
           var productCart = cart.find(product => product.id === action.product.id)
 
@@ -68,11 +68,11 @@ export const reducers = (state = initialState, action) => {
 
         case REMOVE_PRODUCT:
 
-          products = [...state.products]
+          products = JSON.parse(JSON.stringify(state.products))
 
           var shopProduct = products.find(product => product.id === action.product.id)
 
-          cart = [...state.cart]
+          cart = JSON.parse(JSON.stringify(state.cart))
 
           var cartPrduct = cart.find(product => product.id === action.product.id)
 
@@ -99,7 +99,7 @@ export const reducers = (state = initialState, action) => {
 
         case PLUS_PRODUCT:
 
-          products = [...state.products]
+          products = JSON.parse(JSON.stringify(state.products))
 
           shopProduct = products.find(product => product.id === action.product.id)
 
@@ -111,7 +111,7 @@ export const reducers = (state = initialState, action) => {
           
           products.sort((productA,productB) => (productA.id<productB.id ? -1 : (productA.rol > productB.rol) ? 1 : 0))
 
-          cart = [...state.cart]
+          cart = JSON.parse(JSON.stringify(state.cart))
           cart = cart.filter(product => product.id !== action.product.id)
 
           var newProduct = action.product
@@ -135,7 +135,7 @@ export const reducers = (state = initialState, action) => {
 
         case MINUS_PRODUCT: 
         
-          products = [...state.products]
+          products = JSON.parse(JSON.stringify(state.products))
 
           shopProduct = products.find(product => product.id === action.product.id)
 
@@ -147,7 +147,7 @@ export const reducers = (state = initialState, action) => {
           
           products.sort((productA,productB) => (productA.id<productB.id ? -1 : (productA.rol > productB.rol) ? 1 : 0))
 
-          cart = [...state.cart]
+          cart = JSON.parse(JSON.stringify(state.products))
           cart = cart.filter(product => product.id !== action.product.id)
 
           newProduct = action.product
